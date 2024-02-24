2adead52d5bc08bc5e0e70e44c817d9a8a8e8dfb
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-02-24 06:13:57 -0300

    Fx-Studie

---
+++ Curso-em-Vídeo/OlaMundoJava-fx/src/olamundojava/fx/FXMLDocumentController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package olamundojava.fx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Augusto Winkler
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMensagem;
+    private Button btnClock;
+    
+    @FXML
+    private void clickouBotao(ActionEvent event) {
+        lblMensagem.setText("Ola, Mundo!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
