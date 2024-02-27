5ca2ce6540b051c8edfdac707a5c43777ba427e7
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-02-27 17:06:07 -0300

    Operators-Studie

---
+++ Curso-em-Vídeo/OperadorTernario.java
@@ -0,0 +1,17 @@
+package operadorternario;
+
+
+public class OperadorTernario {
+
+  
+    public static void main(String[] args) {
+        int n1,n2,r;
+        n1 = 14;
+        n2 = 8;
+        
+        r = (n1>n2)? n1+n2:n1-n2;
+        System.out.println(r);
+        
+    }
+    
+}
