f4d38f68387b07e5150cfcc2dc599986ddc237c2
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-02-26 21:06:12 -0300

    n

---
+++ Curso-em-Vídeo/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,22 @@
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = ((n1 + n2) /2);
+        System.out.println("A media é igual a " + m);*/
+        
+        int numero = 5;
+        int valor = 5 + numero++;
+        // Operador Unário
+        //numero++;
+        System.out.println(valor);
+    }
+    
+}
