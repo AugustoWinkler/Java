b775c7ff54697d0e58c431bc888be05aa94322f5
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-03-02 21:17:16 -0300

    Create ForRepeticao.java

---
+++ Curso-em-Vídeo/ForRepeticao.java
@@ -0,0 +1,14 @@
+package forrepeticao;
+
+
+public class ForRepeticao {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int cc = 0; cc < 4; cc++){
+            System.out.println("Cambalhota");
+        }
+    }
+    
+}
