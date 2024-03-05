a9d09eea9d289bfe7a8a6df6b187a508983050d4
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-03-05 19:20:40 -0300

    Metodos/Func

---
+++ Curso-em-Vídeo/Metodos.java
@@ -0,0 +1,14 @@
+
+package metodos;
+
+public class Metodos {
+
+   static void soma(int a,int b){
+       int s = a + b;
+       System.out.println("a soma é: " + s);
+   }
+    public static void main(String[] args) {
+        soma(5,2);
+    }
+    
+}
