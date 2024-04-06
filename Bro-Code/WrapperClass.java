1f38d445362597a36f9151ae40783f6c13f2995b
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-04-05 23:50:26 -0300

    WrapperClass

--- Bro-Code/WrapperClass.java
+++ Bro-Code/WrapperClass.java
@@ -3,8 +3,28 @@ package BroCode;
 public class WrapperClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Wrapper class  = provides a way to use primitive data types as reference data type
+		//					reference data types contain useful methods
+		//					can be used with collections (ex.ArrayList)
+		//primivite       //wrapper
+		//--------------------------
+		//boolean 			Boolean
+		//char				Character
+		//int				Integer
+		//double			Double
+		
+		//autoboxing =  the automatic conversion that the Java compiler makes between the primitive and their corresponding object wrapper classes 
+		//unboxing =  the reverse of autoboxing. Automatic conversion of wrapper class primivete
+	
+	Boolean a = true;
+	Character b = '@';
+	Integer c = 123;
+	Double d = 3.14;
+	String e = "Augusto";
+	
 
+	
+	
 	}
 
 }
