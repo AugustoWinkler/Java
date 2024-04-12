0e5302505dedfd30463c0c5401a32a37a319d85d
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-04-11 23:50:10 -0300

    Methods Studie

---
+++ Bro-Code/Method2.java
@@ -0,0 +1,18 @@
+package BroCode;
+
+public class Method2 {
+	
+	public static void main(String[] args) {
+		
+		int x = 3;
+		int y = 4;
+		int result =  add(x,y);
+		
+		System.out.println(result);
+	}
+	
+	static Integer add(int x, int y) {
+		return x + y;
+	}
+
+}
