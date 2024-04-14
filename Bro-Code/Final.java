2cc88ba82f42236d92b0998be99abe2ca13e978f
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-04-13 23:34:47 -0300

    Final

---
+++ Bro-Code/Final.java
@@ -0,0 +1,16 @@
+package BroCode;
+
+public class Final {
+
+	public static void main(String[] args) {
+		
+		//Final cannot be change 
+		
+		final double PI = 3.14159265358979323846264338327950288419716939937510;
+		
+		
+		System.out.println(PI);
+
+	}
+
+}
