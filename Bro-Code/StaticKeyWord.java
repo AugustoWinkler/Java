67f934eb6f3ef723379e1cf5018159c9df782c71
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-04-18 23:14:51 -0300

    StaticKeyword

---
+++ Bro-Code/StaticKeyWord.java
@@ -0,0 +1,34 @@
+package BroCode;
+
+public class StaticKeyWord {
+	
+	public static void main(String[] args) {
+		// static = modifier. A single copy of a variable/method is created and shared.
+		// 			the class "owns" the static member
+		
+		
+		Friend friend1 = new Friend("Sponge bob");
+		Friend friend2 = new Friend("Patrick");
+		
+		
+		Friend.displayFriends();
+		
+		
+		
+	}
+	
+	public static class Friend {
+		String name;
+		static int numberOfFriends;
+		
+		Friend(String name){
+			this.name = name;
+			numberOfFriends++;
+		}
+		
+		static void displayFriends() {
+			System.out.println("You have " + numberOfFriends + " Friends");
+		}
+	}
+
+}
