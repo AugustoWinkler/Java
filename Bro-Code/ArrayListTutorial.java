adda6a28e53ecf998f83a3a18de1c3873b22180f
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-05-02 22:22:51 -0300

    Update ArrayListTutorial.java

--- Bro-Code/ArrayListTutorial.java
+++ Bro-Code/ArrayListTutorial.java
@@ -7,7 +7,7 @@ public class ArrayListTutorial{
 	public static void main(String[] args) {
 		// ArrayList = a resizable array.
 		//			   elements can be added and removed after compilation phase
-		// 			   store reference data types.
+		// 			   store reference data types
 		
 		ArrayList<String> food = new ArrayList<String>(); 
 
