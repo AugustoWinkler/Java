fbe20a38b1aea4f8274759ebd681bfd33e0f3540
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-05-05 21:26:59 -0300

    Update inheritance.java

--- Bro-Code/inheritance.java
+++ Bro-Code/inheritance.java
@@ -3,6 +3,8 @@ package BroCode;
 public class inheritance {
 
 	public static void main(String[] args) {
+				// inheritance = 	the process where one class acquires,
+				//					the attributes and methods of another.
 
 				
 			Car car = new Car();
