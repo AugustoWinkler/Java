bb7b3227f71504d87c2c4626e59a72450edb38d4
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-12-20 22:31:27 -0300

    JavaFX

---
+++ Java/JavaFX/ActionEvent/Controller.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class Controller {
+	
+	
+	@FXML
+	
+	private Circle myCircle;
+	private double x;
+	private double y;
+	
+	public void up(ActionEvent e) {
+
+		myCircle.setCenterY(y-=3);
+	}
+	public void right(ActionEvent e) {
+
+		myCircle.setCenterX(x+=3);
+	}
+	public void down(ActionEvent e) {
+		myCircle.setCenterY(y+=3);
+	}
+	public void left(ActionEvent e) {
+		System.out.println("Left");
+		myCircle.setCenterX(x-=3);
+	}
+
+
+
+
+}
